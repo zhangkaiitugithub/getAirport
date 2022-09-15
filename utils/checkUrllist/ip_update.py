dda611d0c6b7ce9762ce8504d4bf575f9fe131f3
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-09-15 12:54:45 +0800

    Add files via upload

---
+++ utils/checkUrllist/ip_update.py
@@ -0,0 +1,19 @@
+from urllib import request          # Urllib是python内置的HTTP请求库,urllib.request获取URL的Python模块
+
+#Country.mmdb 下载地址
+geoip_url = 'https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb'
+
+#根据自己情况设置mmdb存放位置
+countrymmdb_file = './Country.mmdb'
+
+def geoip_update(url):                  # 更新IP位置数据库存放到  `./utils/Country.mmdb`
+    print('Downloading Country.mmdb...')
+    try:
+        request.urlretrieve(url, countrymmdb_file)
+        print('Downloading Country.mmdb Success!\n')
+    except Exception:
+        print('Downloading Country.mmdb Failed!\n')
+        pass
+
+if __name__ == '__main__':
+    geoip_update(geoip_url)                           # 更新IP位置数据库
