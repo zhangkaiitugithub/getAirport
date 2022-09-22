44418fc1a4f523e145e7d86c507e0a5dc4a99272
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-09-22 13:19:21 +0800

    Update check.py

--- utils/checkUrllist/check.py
+++ utils/checkUrllist/check.py
@@ -23,7 +23,7 @@ def list_rm(url_list):  #列表去重
                 length -= 1
             begin_2 += 1
         begin += 1
-    print(f'\n-----去重开始-----\n')
+    print(f'\n-----去重结束-----\n')
     return url_list
     
 def sub_check(url,bar):
